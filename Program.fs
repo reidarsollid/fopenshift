d408811b3d2f79608f8fef7306dce16a1fbfba9a
reidarsollid <rsollid@gmail.com>
2019-03-20 20:27:48 +0100

    Added Giraffe and ASP

--- Program.fs
+++ Program.fs
@@ -1,8 +1,28 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+open Giraffe
 open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
 
+let routes =
+    choose [
+        route "/" >=> text "Hello World from F# dotnetcore"
+    ]
+
+let configureApp(app: IApplicationBuilder) =
+    app.UseGiraffe routes
+    
+let configureServices(services: IServiceCollection) =
+    services.AddGiraffe() |> ignore
+ 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
+let main _ =
+    WebHostBuilder()
+        .UseKestrel()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .Build()
+        .Run()
     0 // return an integer exit code
